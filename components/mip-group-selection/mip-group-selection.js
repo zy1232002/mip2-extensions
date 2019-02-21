e9c956bca00b1b507049baac5aa659ae6da19ec2
guozhuorong <guozhuorong@baidu.com>
2019-02-21 12:41:10 +0800

    give it a shot

--- components/mip-group-selection/mip-group-selection.js
+++ components/mip-group-selection/mip-group-selection.js
@@ -9,7 +9,7 @@ const {
 } = MIP
 
 const log = util.log('mip-group-selection')
-const { fixedElement } = viewer
+// const { fixedElement } = viewer
 
 export default class MIPGroupSelection extends CustomElement {
   /**
@@ -17,45 +17,34 @@ export default class MIPGroupSelection extends CustomElement {
    *
    * @returns {Object} Promise
    */
-  // async getData () {
-  //   if (this.dataUrl) {
-  //     try {
-  //       let res = await fetch(this.dataUrl, { credentials: 'include' })
-  //       if (res.ok) {
-  //         return res.json()
-  //       }
-  //       return Promise.reject(new Error('mip-city-selection 组件 Fetch 请求失败!'))
-  //     } catch (e) {
-  //       return Promise.reject(new Error('mip-city-selection 组件 Fetch 请求失败!'))
-  //     }
-  //   }
-  //   let groupData = this.element.querySelector('script[type="application/json"]')
-  //   if (groupData) {
-  //     try {
-  //       groupData = util.jsonParse(groupData.textContent)
-  //     } catch (e) {
-  //       return Promise.reject(new Error('mip-city-selection 组件 json 配置错误, 请检查 json 格式。'))
-  //     }
-  //     return groupData
-  //   }
-  //   return null
-  // }
-  getData () {
+  async getData () {
+    if (this.dataUrl) {
+      try {
+        let res = await fetch(this.dataUrl, { credentials: 'include' })
+        if (res.ok) {
+          return res.json()
+        }
+        return Promise.reject(new Error('mip-city-selection 组件 Fetch 请求失败!'))
+      } catch (e) {
+        return Promise.reject(new Error('mip-city-selection 组件 Fetch 请求失败!'))
+      }
+    }
     let groupData = this.element.querySelector('script[type="application/json"]')
-    return new Promise((resolve, reject) => {
+    if (groupData) {
       try {
         groupData = util.jsonParse(groupData.textContent)
       } catch (e) {
-        reject(new Error('asd'))
+        return Promise.reject(new Error('mip-city-selection 组件 json 配置错误, 请检查 json 格式。'))
       }
-      resolve(groupData)
-    })
+      return groupData
+    }
+    return null
   }
 
   /**
    * 获取分组配置信息，渲染备选项和右侧快速选择列表
    *
-   * @param {Object} html 要被渲染的数据
+   * @param {string} html templates 解析出来的字符串
    */
   renderHtml (html) {
     let wrapper = document.createElement('div')
@@ -75,24 +64,24 @@ export default class MIPGroupSelection extends CustomElement {
   /**
    * 绑定侧边栏快捷选择事件
    */
-  bindSidebarClickEvent () {
-    // ios sf 环境中
-    if (!MIP.standalone && util.platform.isIOS() && fixedElement._fixedLayer) {
-      let wrapper = fixedElement._fixedLayer.querySelector('.mip-group-selection-sidebar-wrapper')
-      util.event.delegate(wrapper, '.mip-group-selection-link', 'click', e => {
-        let button = e.target
-        let targetAnchor = button.dataset.targetAnchor
-        // 滚动待选列表到指定分组
-        this.scrollToAnchor(targetAnchor)
-      })
-    } else {
-      util.event.delegate(this.element, '.mip-group-selection-link', 'click', e => {
-        let button = e.target
-        let targetAnchor = button.dataset.targetAnchor
-        this.scrollToAnchor(targetAnchor)
-      })
-    }
-  }
+  // bindSidebarClickEvent () {
+  //   // ios sf 环境中
+  //   if (!MIP.standalone && util.platform.isIOS() && fixedElement._fixedLayer) {
+  //     let wrapper = fixedElement._fixedLayer.querySelector('.mip-group-selection-sidebar-wrapper')
+  //     util.event.delegate(wrapper, '.mip-group-selection-link', 'click', e => {
+  //       let button = e.target
+  //       let targetAnchor = button.dataset.targetAnchor
+  //       // 滚动待选列表到指定分组
+  //       this.scrollToAnchor(targetAnchor)
+  //     })
+  //   } else {
+  //     util.event.delegate(this.element, '.mip-group-selection-link', 'click', e => {
+  //       let button = e.target
+  //       let targetAnchor = button.dataset.targetAnchor
+  //       this.scrollToAnchor(targetAnchor)
+  //     })
+  //   }
+  // }
 
   /**
    * 滚动待选列表到指定分组
@@ -112,14 +101,14 @@ export default class MIPGroupSelection extends CustomElement {
    * 选项绑定点击事件，点击修改 MIP data 并触发 selected 事件
    *
    */
-  bindItemClickEvent () {
-    util.event.delegate(this.element, '.mip-group-selection-item', 'click', e => {
-      let itemData = e.target && e.target.dataset
-      e.data = itemData
-      MIP.setData(e.data)
-      viewer.eventAction.execute('selected', this.element, e)
-    })
-  }
+  // bindItemClickEvent () {
+  //   util.event.delegate(this.element, '.mip-group-selection-item', 'click', e => {
+  //     let itemData = e.target && e.target.dataset
+  //     e.data = itemData
+  //     MIP.setData(e.data)
+  //     viewer.eventAction.execute('selected', this.element, e)
+  //   })
+  // }
 
   firstInviewCallback () {
     let el = this.element
@@ -135,9 +124,9 @@ export default class MIPGroupSelection extends CustomElement {
       // 修改最下方分组的样式，增加 marginBottom, 保证滚动后分组标题可以在页面最上方
       this.modifyMarginBottom()
       // 绑定侧边栏快捷选择事件
-      this.bindSidebarClickEvent()
+      // this.bindSidebarClickEvent()
       // 绑定列表元素选择事件
-      this.bindItemClickEvent()
+      // this.bindItemClickEvent()
     }).catch(log.warn)
   }
 }
